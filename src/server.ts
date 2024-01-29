2add65d5373bb975a34d1c1ce715b7d73247e837
Computewell <ololade220@gmail.com>
2024-01-29 12:41:45 +0100

    Server Created

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,5 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
-
-
-
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
